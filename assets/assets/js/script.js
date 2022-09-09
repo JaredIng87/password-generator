b38337d9d4021c558f5172d5cd8944cd3d3eccc3
JaredIng87 <jaredingersoll14@gmail.com>
2022-09-08 22:46:05 -0400

    create arrays with different variable types

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -1,4 +1,12 @@
 // Assignment code here
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var special = ["!", "$", "#", "?", "&"];
+var index = [];
+var userInput = '';
+
+
 
 
 // Get references to the #generate element
@@ -8,7 +16,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  generatePassword();
   passwordText.value = password;
 
 }
