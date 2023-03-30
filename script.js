eb5703d04f2a496920e7d877a9e25b810043b47a
JaredIng87 <jaredingersoll14@gmail.com>
2023-03-30 13:41:15 -0400

    debug

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //arrays of includable characters
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowerCase = upperCase.map(letter => letter.toLowerCase());
 var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var special = ["!", "$", "#", "?", "&"];
 var userChoices = [];
-var length = '';
+
 
 //function to generate randomized password
 var generatePassword = function() {
@@ -60,11 +60,10 @@ var generatePassword = function() {
 
   //call all functions
   passwordLength();
-  includeLow();
   includeUp();
+  includeLow();
   includeNum();
   includeSpec();
-//TODO: determine why windows call twice
 
 //for loop with math to create random string including chosen characters at chosen length
 var randomString = '';
@@ -74,28 +73,22 @@ var randomInteger = userChoices[math];
 randomString += randomInteger;
 }
 
-
 //return generated string
 return randomString;
 }
 
 
-
-
-
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  generatePassword();
+  // generatePassword();
   passwordText.value = password;
-
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
